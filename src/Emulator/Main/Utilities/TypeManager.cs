a194341a6b32b73a58e3ae0fd54a66f6976464fb
Jon Lamb <jon@auxon.io>
2025-12-03 07:41:00 -0800

    Remove lucene logger backend

--- src/Emulator/Main/Utilities/TypeManager.cs
+++ src/Emulator/Main/Utilities/TypeManager.cs
@@ -786,7 +786,6 @@ namespace Antmicro.Renode.Utilities
             "IronPython.StdLib.dll",
             "IronPython.Wpf.dll",
             "libtftp.dll",
-            "Lucene.Net.dll",
             "LZ4.dll",
             "mcs.dll",
             "Microsoft.Dynamic.dll",
